b9a5cca0d0d70722cc3b8f7467fc1840a37df9f2
lenovo <940045102@qq.com>
2021-02-19 14:57:27 +0800

    111

--- oauth/src/main/java/com/tingbei/oauth/service/UserLoginService.java
+++ oauth/src/main/java/com/tingbei/oauth/service/UserLoginService.java
@@ -3,9 +3,13 @@ package com.tingbei.oauth.service;/**
  * @Date:2021/2/19 13:17
  */
 
+import com.tingbei.common.vo.UserInfoVO;
+
 /**
  *@Author:JXW
  *@Date:2021/2/19 13:17
  */
 public interface UserLoginService {
+
+    UserInfoVO queryOneUserInfo(String loginName, String password);
 }

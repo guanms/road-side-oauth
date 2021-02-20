bc6ba84a683270ab59db826f4371a9aeb98e8d5c
guanshuai <215216569@qq.com>
2021-02-20 17:12:36 +0800

    1234

--- oauth/src/main/java/com/tingbei/oauth/service/OauthTokenRequestService.java
+++ oauth/src/main/java/com/tingbei/oauth/service/OauthTokenRequestService.java
@@ -7,7 +7,7 @@ import com.tingbei.common.vo.AuthTokenInfo;
 import com.tingbei.common.vo.OauthTokenRequestVO;
 
 /**
- *@Author:JXW
+ *@Author:JXW122121212121212121
  *@Date:2021/2/19 16:23
  */
 public interface OauthTokenRequestService {

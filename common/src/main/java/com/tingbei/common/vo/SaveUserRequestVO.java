92be513fe6cead1d71b193c7c317dacd2661e0ac
guanshuai <215216569@qq.com>
2021-02-20 22:20:02 +0800

    1234

---
+++ common/src/main/java/com/tingbei/common/vo/SaveUserRequestVO.java
@@ -0,0 +1,33 @@
+package com.tingbei.common.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class SaveUserRequestVO implements Serializable {
+
+    private static final long serialVersionUID = -6825426039744511010L;
+    /**
+     * 用户唯一标识，用于之后的角色查询
+     */
+    private String uiUuid;
+    private String userName;
+    private String loginName;
+    private String userAttribute;
+    private String userStatus;
+    private String userCreateMan;
+    private String userCreateTime;
+    private String identityCode;
+    private Integer areaCode;
+    private String specialMark;
+    private String userType;
+    /**
+     * 页面新增用户时候需要password
+     */
+    private String password;
+    /**
+     * rediskey
+     */
+    private String redisId;
+}

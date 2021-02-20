60c8becfe1315b7cdc46bdf314d1374887b8acb7
guanshuai <215216569@qq.com>
2021-02-20 17:48:25 +0800

    1234

--- signup/src/main/java/com/tingbei/signup/SignupApplication.java
+++ signup/src/main/java/com/tingbei/signup/SignupApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.tingbei")
 @MapperScan("com.tingbei.common.repository")
 @EnableDiscoveryClient
 @EnableFeignClients

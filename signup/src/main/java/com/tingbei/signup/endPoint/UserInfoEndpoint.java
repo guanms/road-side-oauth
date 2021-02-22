9aef1979b8e2a3ad6f82fa528eeee502fa00ad32
guanshuai <215216569@qq.com>
2021-02-22 11:46:13 +0800

    1234

--- signup/src/main/java/com/tingbei/signup/endPoint/UserInfoEndpoint.java
+++ signup/src/main/java/com/tingbei/signup/endPoint/UserInfoEndpoint.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/userInfo/operate")
 public class UserInfoEndpoint {
